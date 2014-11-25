91ed7ff6dd442b57fd97ee01763804dc748d7feb
Timothy Kua <timothyk7@gmail.com>
2014-11-25 12:14:27 -0800

    Added collision detection utility

--- Space Race/Energy.swift
+++ Space Race/Energy.swift
@@ -1,24 +1,22 @@
 //
-//  SpaceShip.swift
+//  Energy.swift
 //  Space Race
 //
-//  Created by Max Takano on 11/13/14.
+//  Created by Tim K on 11/23/14.
 //  Copyright (c) 2014 Max Takano. All rights reserved.
 //
 
+import Foundation
 import SpriteKit
 
-class SpaceShip:SKSpriteNode {
-    let startPosition = CGPoint(x: viewSize.width * 0.5, y: viewSize.height * 0.2)
-    let nameShip = "NoobShip"
-    
-    var turnSpeed = 100
-    var forwardSpeed = 50
+class Energy:SKSpriteNode {
+    //    let startPosition = CGPoint(x: viewSize.width * 0.5, y: viewSize.height * 0.2)
+    let nameEnergy = "Energy"
     
     override init(texture: SKTexture!, color: UIColor!, size: CGSize) {
         super.init(texture: texture, color: color, size: size)
         
-        self.shipSetup()
+        self.energySetup()
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -26,16 +24,19 @@ class SpaceShip:SKSpriteNode {
     }
     
     
-    func shipSetup() {
+    func energySetup() {
         // Texture Properties
         self.texture?.filteringMode = SKTextureFilteringMode.Nearest
         
         // Position
-        self.position = startPosition
+        //        self.position = startPosition
+        let randomX = getRandom(min: CGFloat(0.0), CGFloat(1.0))
+        self.position = CGPoint(x: viewSize.width * randomX, y:viewSize.height)
+        self.zRotation = getRandom(min: CGFloat(0.0), CGFloat(6.28))
         self.zPosition = GameLayer.Game
         
         // Other Properites
-        self.name = nameShip
+        self.name = nameEnergy
         
         // Physics
         self.physicsBody = SKPhysicsBody(circleOfRadius: self.size.height / 2)
@@ -44,10 +45,15 @@ class SpaceShip:SKSpriteNode {
         self.physicsBody?.linearDamping = 1.0
         self.physicsBody?.angularDamping = 1.0
         self.physicsBody?.affectedByGravity = false
-        self.physicsBody?.categoryBitMask = Contact.Ship
-        self.physicsBody?.collisionBitMask = Contact.Asteroid | Contact.Energy
-        self.physicsBody?.contactTestBitMask = Contact.Asteroid | Contact.Energy
-//        self.physicsBody?.collisionBitMask = Contact.Alien | Contact.AlienMissile | Contact.AlienTorpedo
-//        self.physicsBody?.contactTestBitMask = Contact.Alien | Contact.AlienMissile | Contact.AlienTorpedo
+        self.physicsBody?.categoryBitMask = Contact.Energy
+        
+        self.physicsBody?.collisionBitMask = Contact.Ship
+        self.physicsBody?.contactTestBitMask = Contact.Ship
+        
+    }
+    
+    
+    func updateVelocity(newVelocity: Int) {
+        self.physicsBody?.velocity = CGVectorMake(0.0, -1.0 * CGFloat(newVelocity) )
     }
 }
